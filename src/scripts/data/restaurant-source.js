5090aba3fb16a06ec1d8f0f08e118dfca145d344
gerrygeop <gerryja28@gmail.com>
2021-06-12 17:57:13 +0800

    Add function sending review

--- src/scripts/data/restaurant-source.js
+++ src/scripts/data/restaurant-source.js
@@ -14,8 +14,17 @@ class RestaurantSource {
     return responJson.restaurant;
   }
 
-  static async sendingReview(review) {
-    // TODO: Add...fetch to json
+  static async sendingReview(data) {
+    const response = await fetch(API_ENDPOINT.REVIEW, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Auth-Token': CONFIG.KEY,
+      },
+      body: data,
+    });
+    const responseJson = await response.json();
+    return responseJson;
   }
 }
 
