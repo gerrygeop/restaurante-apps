d1cf2e2310a83f971ce235d1c17abdd1198d4559
gerrygeop <gerryja28@gmail.com>
2021-07-01 04:55:13 +0800

    :space_invader: add codecept

---
+++ codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: 'e2e/**/*.spec.js',
+  output: 'e2e/outputs',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:8081',
+      show: true,
+      windowSize: 'y',
+    },
+  },
+  include: {
+    I: './steps_file.js',
+  },
+  bootstrap: null,
+  mocha: {},
+  name: '55-restaurant-apps',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,6 @@
+/* eslint-disable new-cap */
+Feature('Liking Restaurant');
+
+Scenario('test something', ({ I }) => {
+
+});
