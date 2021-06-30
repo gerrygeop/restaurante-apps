0a713f1c88f536f7aecffccb6a869bc86dccc951
gerrygeop <gerryja28@gmail.com>
2021-07-01 05:10:44 +0800

    :zap: create e2e test

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,6 +1,11 @@
 /* eslint-disable new-cap */
 Feature('Liking Restaurant');
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
 
+Scenario('showing empty liked restaurant', ({ I }) => {
+  I.seeElement('restaurant-list');
+  I.see(`You haven't added your favorite restaurant yet`, '.empty-favorite');
 });
