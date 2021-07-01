7945e9ba06c9903c7dacfa6f5ba60b06e59e1310
gerrygeop <gerryja28@gmail.com>
2021-07-01 23:17:34 +0800

    :recycle: liking restaurant e2e test

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 /* eslint-disable new-cap */
 Feature('Liking Restaurant');
 
@@ -5,7 +7,23 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
-Scenario('showing empty liked restaurant', ({ I }) => {
-  I.seeElement('restaurant-list');
+Scenario('Liking one restaurant', async ({ I }) => {
   I.see(`You haven't added your favorite restaurant yet`, '.empty-favorite');
+  I.amOnPage('/');
+
+  I.seeElement('.card-title a');
+
+  const firstRestaurant = locate('.card-title a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('restaurant-item');
+
+  const favoriteRestaurantName = await I.grabTextFrom('.card-title a');
+
+  assert.strictEqual(firstRestaurantName, favoriteRestaurantName);
 });
