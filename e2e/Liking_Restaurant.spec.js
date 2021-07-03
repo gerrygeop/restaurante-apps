c00e34b7c34248da314a5eb47d64301fe7d2a9dd
gerrygeop <gerryja28@gmail.com>
2021-07-03 19:13:03 +0800

    :cake: error e2e test

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,14 +1,15 @@
 const assert = require('assert');
 
 /* eslint-disable new-cap */
-Feature('Liking Restaurant');
+Feature('Restaurant Features');
 
 Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
 Scenario('Liking one restaurant', async ({ I }) => {
-  I.see(`You haven't added your favorite restaurant yet`, '.empty-favorite');
+  I.dontSeeElement('restaurant-item');
+
   I.amOnPage('/');
 
   I.seeElement('.card-title a');
@@ -21,9 +22,36 @@ Scenario('Liking one restaurant', async ({ I }) => {
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
-  I.seeElement('restaurant-item');
+  I.seeElement('.card-title a');
 
   const favoriteRestaurantName = await I.grabTextFrom('.card-title a');
 
   assert.strictEqual(firstRestaurantName, favoriteRestaurantName);
+  I.see(favoriteRestaurantName);
+});
+
+Scenario('Unliking one restaurant', async ({ I }) => {
+  I.see(`You haven't added your favorite restaurant yet`, '.empty-favorite');
+
+  I.amOnPage('/');
+
+  I.seeElement('.card-title a');
+
+  const firstRestaurant = locate('.card-title a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card-title a');
+  const favoriteRestaurantName = await I.grabTextFrom('.card-title a');
+
+  assert.strictEqual(firstRestaurantName, favoriteRestaurantName);
+
+  I.click(favoriteRestaurantName);
+  I.seeElement('#likeButton');
+
+  I.amOnPage('/#/favorite');
 });
