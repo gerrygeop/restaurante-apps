f7207bd820b1402ca784828406a4910e194b21a9
gerrygeop <gerryja28@gmail.com>
2021-07-05 16:14:08 +0800

    :zap: image responsive with sharp

--- webpack.common.js
+++ webpack.common.js
@@ -45,6 +45,9 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/**'],
+          },
         },
       ],
     }),
